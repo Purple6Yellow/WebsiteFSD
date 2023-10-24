a4b01f213c8918fbc5bf58b434719904a00a0410
Purple6Yellow <vikamper@hotmail.com>
2023-10-24 13:05:26 +0200

    tweede push'

--- mysite/blog/models.py
+++ mysite/blog/models.py
@@ -3,18 +3,25 @@ from django.db import models
 from django.utils import timezone
 
 class Post(models.Model):
-    #Datum = models.DateField(default=timezone.now)
-    Title = models.CharField(max_length=200)
-    Tijdstip_aanvang = models.TimeField(blank=True,null =True)
-    Tijdstip_einde = models.TimeField(blank=True,null =True)
-    Omschrijving = models.TextField()
-    Kosten = models.DecimalField(decimal_places=2,max_digits=10)
-    published_date=models.DateField(default=timezone.now)
-    
+    author = models.ForeignKey (settings.AUTH_USER_MODEL,on_delete=models.CASCADE)
+    title=models.CharField(max_length=200)
+    text = models.TextField()
+    created_date = models.DateTimeField(default=timezone.now)
+    published_date = models.DateTimeField(blank=True, null=True)
     
     def publish(self):
        self.published_date = timezone.now()
        self.save()
 
     def __str__(self):
-       return self.published_date
+       return self.title
+
+#class Post(models.Model):
+    #Datum = models.DateField(default=timezone.now)
+    #Title = models.CharField(max_length=200)
+    #Tijdstip_aanvang = models.TimeField(blank=True,null =True)
+    #Tijdstip_einde = models.TimeField(blank=True,null =True)
+    #Omschrijving = models.TextField()
+    #Kosten = models.DecimalField(decimal_places=2,max_digits=10, default='')
+    #published_date=models.DateField(default=timezone.now)
+    
